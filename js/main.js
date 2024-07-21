d9b10dca6a22db4b7380365b52c2da6ab31fd419
lejbman <lejbman@yahoo.com.ar>
2024-07-21 19:10:33 -0300

    Ajustes en local storage

--- js/main.js
+++ js/main.js
@@ -1,42 +1,74 @@
 // Array para almacenar empleados
-const empleadosarts = [];
+let empleadosarts = [];
+
+// Función para guardar empleados en localStorage
+function guardarEmpleadosEnLocalStorage() {
+  try {
+    const empleadosJson = JSON.stringify(empleadosarts);
+    localStorage.setItem('empleados', empleadosJson);
+    console.log('Empleados guardados en localStorage correctamente.');
+  } catch (error) {
+    console.error('Error al guardar empleados en localStorage:', error);
+  }
+}
+
+// Función para recuperar empleados desde localStorage
+function recuperarEmpleadosDeLocalStorage() {
+  try {
+    const empleadosJson = localStorage.getItem('empleados');
+    if (empleadosJson) {
+      empleadosarts = JSON.parse(empleadosJson);
+      console.log('Empleados recuperados desde localStorage:', empleadosarts);
+    } else {
+      empleadosarts = [];
+      console.log('No se encontraron empleados en localStorage. Se inicializa como un array vacío.');
+    }
+  } catch (error) {
+    console.error('Error al recuperar empleados desde localStorage:', error);
+    empleadosarts = [];
+  }
+}
+
+// Llamada para recuperar empleados al cargar la página
+document.addEventListener('DOMContentLoaded', function() {
+  recuperarEmpleadosDeLocalStorage();
+});
 
 // Calcular cotización por cantidad de empleados y riesgo de la actividad
 const calcularButton = document.getElementById('calcularButton');
 calcularButton.addEventListener('click', function() {
-    const numEmpleados = parseInt(document.getElementById('empleados').value);
-    const factorRiesgo = parseFloat(document.getElementById('riesgo').value);
+  const numEmpleados = parseInt(document.getElementById('empleados').value);
+  const factorRiesgo = parseFloat(document.getElementById('riesgo').value);
 
-    const cotizacion = calcularCotizacion(numEmpleados, factorRiesgo);
+  const cotizacion = calcularCotizacion(numEmpleados, factorRiesgo);
 
-    const resultadoElement = document.getElementById('resultado');
-    resultadoElement.textContent = `La cotización mensual es: $${cotizacion.toFixed(2)}`;
+  const resultadoElement = document.getElementById('resultado');
+  resultadoElement.textContent = `La cotización mensual es: $${cotizacion.toFixed(2)}`;
 });
 
 function calcularCotizacion(numEmpleados, factorRiesgo) {
-    const costoBasePorEmpleado = 1000; // Costo base por cada empleado
-    const porcentajeFactorRiesgo = factorRiesgo / 100;
-    const costoTotal = numEmpleados * costoBasePorEmpleado * (1 + porcentajeFactorRiesgo);
-    return costoTotal;
+  const costoBasePorEmpleado = 1000; // Costo base por cada empleado
+  const porcentajeFactorRiesgo = factorRiesgo / 100;
+  const costoTotal = numEmpleados * costoBasePorEmpleado * (1 + porcentajeFactorRiesgo);
+  return costoTotal;
 }
 
 document.getElementById('cargarEmpleadoButton').addEventListener('click', function() {
-  
   // Obtener los valores de los campos del formulario
   const cargarNombre = document.getElementById('nombre').value;
-  const cargarCuit = document.getElementById('cuit').value.trim(); // Obtener el valor del campo CUIT
-  const cargarEdad = document.getElementById('edad').value.trim(); // Obtener el valor del campo edad
+  const cargarCuit = document.getElementById('cuit').value.trim();
+  const cargarEdad = document.getElementById('edad').value.trim();
   const cargarOcupacion = document.getElementById('ocupacion').value;
 
   // Validar que cargarCuit y cargarEdad sean números válidos y no negativos
   if (!esNumeroValido(cargarCuit) || parseInt(cargarCuit, 10) < 0) {
-      mostrarError('El CUIT debe ser un número válido y no puede ser negativo.');
-      return;
+    mostrarError('El CUIT debe ser un número válido y no puede ser negativo.');
+    return;
   }
 
   if (!esNumeroValido(cargarEdad) || parseInt(cargarEdad, 10) < 0) {
-      mostrarError('La edad debe ser un número válido y no puede ser negativa.');
-      return;
+    mostrarError('La edad debe ser un número válido y no puede ser negativa.');
+    return;
   }
 
   // Convertir los valores a números enteros
@@ -45,9 +77,12 @@ document.getElementById('cargarEmpleadoButton').addEventListener('click', functi
 
   // Crear un nuevo objeto Empleado con los valores obtenidos
   const empleado = new Empleado(cargarNombre, cuitNumero, edadNumero, cargarOcupacion);
-  
+
   // Agregar el nuevo empleado al arreglo empleadosarts
   empleadosarts.push(empleado);
+
+  // Guardar empleados en localStorage después de agregar uno nuevo
+  guardarEmpleadosEnLocalStorage();
 
   // Limpiar los campos del formulario después de cargar el empleado
   document.getElementById('nombre').value = '';
@@ -62,20 +97,21 @@ document.getElementById('cargarEmpleadoButton').addEventListener('click', functi
 // Función auxiliar para verificar si un valor es un número válido
 function esNumeroValido(valor) {
   // Verifica que el valor contenga solo dígitos
-  return /^\d+$/.test(valor); 
+  return /^\d+$/.test(valor);
 }
 
 // Función para mostrar mensajes de error en la interfaz
 function mostrarError(mensaje) {
   const errorDiv = document.getElementById('errorDiv');
   errorDiv.textContent = mensaje;
-  errorDiv.style.display = 'block';  // Mostrar el div de error
+  // Mostrar el div de error
+  errorDiv.style.display = 'block';
   setTimeout(() => {
     errorDiv.textContent = '';
-    errorDiv.style.display = 'none';  // Ocultar el div de error después de 3 segundos
+    // Ocultar el div de error después de 3 segundos
+    errorDiv.style.display = 'none';
   }, 3000);
 }
-
 
 // Función para actualizar la lista de empleados en el DOM
 function actualizarListaEmpleados() {
@@ -96,7 +132,6 @@ function actualizarListaEmpleados() {
   });
 }
 
-
 // Clase Empleado
 class Empleado {
   constructor(nombre, cuit, edad, ocupacion) {
@@ -109,27 +144,27 @@ class Empleado {
 
 // Event listener para el botón de búsqueda de empleados por CUIT
 document.getElementById('buscarButton').addEventListener('click', function() {
-    // Obtener el valor ingresado por el usuario
-    const buscar = parseInt(document.getElementById('buscarCuit').value);
-  
-    // Realizar la búsqueda usando find()
-    const busqueda = empleadosarts.find((empleado) => empleado.cuit === buscar);
-  
-    // Mostrar el resultado en el DOM
-    const resultadoBusqueda = document.getElementById('resultadoBusqueda');
-    if (busqueda) {
-      resultadoBusqueda.innerHTML = `
-        <p><strong>Empleado Encontrado:</strong></p>
-        <p><strong>Nombre:</strong> ${busqueda.nombre}</p>
-        <p><strong>CUIT/CUIL:</strong> ${busqueda.cuit}</p>
-        <p><strong>Edad:</strong> ${busqueda.edad}</p>
-        <p><strong>Ocupación:</strong> ${busqueda.ocupacion}</p>
-      `;
-    } else {
-      resultadoBusqueda.textContent = 'No se encontró ningún empleado con ese CUIT/CUIL.';
-    }
+  // Obtener el valor ingresado por el usuario
+  const buscar = parseInt(document.getElementById('buscarCuit').value);
+
+  // Realizar la búsqueda usando find()
+  const busqueda = empleadosarts.find((empleado) => empleado.cuit === buscar);
+
+  // Mostrar el resultado en el DOM
+  const resultadoBusqueda = document.getElementById('resultadoBusqueda');
+  if (busqueda) {
+    resultadoBusqueda.innerHTML = `
+      <p><strong>Empleado Encontrado:</strong></p>
+      <p><strong>Nombre:</strong> ${busqueda.nombre}</p>
+      <p><strong>CUIT/CUIL:</strong> ${busqueda.cuit}</p>
+      <p><strong>Edad:</strong> ${busqueda.edad}</p>
+      <p><strong>Ocupación:</strong> ${busqueda.ocupacion}</p>
+    `;
+  } else {
+    resultadoBusqueda.textContent = 'No se encontró ningún empleado con ese CUIT/CUIL.';
+  }
 });
-  
+
 // Event listener para el botón de filtrar empleados próximos a jubilarse
 document.getElementById('filtrarJubiladosButton').addEventListener('click', function() {
   // Filtrar empleados con edad >= 60
@@ -139,55 +174,59 @@ document.getElementById('filtrarJubiladosButton').addEventListener('click', func
   mostrarResultadosFiltrados(filtrados, 'Empleados próximos a jubilarse (edad >= 60 años)');
 });
 
-  // Event listener para el botón de filtrar empleados menores de 60 años
-  document.getElementById('filtrarNoJubiladosButton').addEventListener('click', function() {
-    // Filtrar empleados con edad < 60
-    const filtrados = empleadosarts.filter((empleado) => empleado.edad < 60);
-  
-    // Mostrar los resultados en el DOM
-    mostrarResultadosFiltrados(filtrados, 'Empleados menores de 60 años');
-});
-  
-  // Función para mostrar los resultados filtrados en el DOM
-  function mostrarResultadosFiltrados(empleadosFiltrados, mensaje) {
-    const resultadosContainer = document.getElementById('resultadosFiltrados');
-    resultadosContainer.innerHTML = '';
-  
-    if (empleadosFiltrados.length > 0) {
-      const header = document.createElement('h3');
-      header.textContent = mensaje;
-      resultadosContainer.appendChild(header);
-  
-      const listaEmpleados = document.createElement('ul');
-      empleadosFiltrados.forEach((empleado) => {
-        const itemEmpleado = document.createElement('li');
-        itemEmpleado.textContent = `Nombre: ${empleado.nombre} - Edad: ${empleado.edad} - CUIT: ${empleado.cuit} - Ocupacion: ${empleado.ocupacion}`;
-        listaEmpleados.appendChild(itemEmpleado);
-      });
-  
-      resultadosContainer.appendChild(listaEmpleados);
-    } else {
-      resultadosContainer.textContent = 'No se encontraron empleados con los criterios especificados.';
-    }
-}
-  
-// filter con includes, que permite al usuario filtrar todos los objetos con un determinado nombre y/o apellido
-// Event listener para el botón de filtrar
-document.getElementById('filtrarButton').addEventListener('click', function() {
-    // Obtener el valor ingresado por el usuario
-    const filtro = document.getElementById('filtroNombreApellido').value.toLowerCase().trim();
-  
-    // Filtrar empleados por nombre y/o apellido
-    const filtrados = empleadosarts.filter((empleado) => {
-      const nombreApellido = `${empleado.nombre} ${empleado.apellido}`.toLowerCase();
-      return nombreApellido.includes(filtro);
-    });
-  
-    // Mostrar los resultados en el DOM
-    mostrarResultadosFiltradosNombreApellido(filtrados);
+// Event listener para el botón de filtrar empleados menores de 60 años
+document.getElementById('filtrarNoJubiladosButton').addEventListener('click', function() {
+  // Filtrar empleados con edad < 60
+  const filtrados = empleadosarts.filter((empleado) => empleado.edad < 60);
+
+  // Mostrar los resultados en el DOM
+  mostrarResultadosFiltrados(filtrados, 'Empleados menores de 60 años');
 });
 
 // Función para mostrar los resultados filtrados en el DOM
+function mostrarResultadosFiltrados(empleadosFiltrados, mensaje) {
+  const resultadosContainer = document.getElementById('resultadosFiltrados');
+  resultadosContainer.innerHTML = '';
+
+  if (empleadosFiltrados.length > 0) {
+    const header = document.createElement('h3');
+    header.textContent = mensaje;
+    resultadosContainer.appendChild(header);
+
+    const listaEmpleados = document.createElement('ul');
+    empleadosFiltrados.forEach((empleado) => {
+      const itemEmpleado = document.createElement('li');
+      itemEmpleado.innerHTML = `
+        <strong>Nombre:</strong> ${empleado.nombre} <br>
+        <strong>CUIT/CUIL:</strong> ${empleado.cuit} <br>
+        <strong>Edad:</strong> ${empleado.edad} <br>
+        <strong>Ocupación:</strong> ${empleado.ocupacion}
+      `;
+      listaEmpleados.appendChild(itemEmpleado);
+    });
+
+    resultadosContainer.appendChild(listaEmpleados);
+  } else {
+    resultadosContainer.textContent = 'No se encontraron empleados con los criterios especificados.';
+  }
+}
+
+// Event listener para el botón de filtrar por nombre y/o apellido
+document.getElementById('filtrarButton').addEventListener('click', function() {
+  // Obtener el valor ingresado por el usuario
+  const filtro = document.getElementById('filtroNombreApellido').value.toLowerCase().trim();
+
+  // Filtrar empleados por nombre y/o apellido
+  const filtrados = empleadosarts.filter((empleado) => {
+    const nombreApellido = `${empleado.nombre} ${empleado.apellido}`.toLowerCase();
+    return nombreApellido.includes(filtro);
+  });
+
+  // Mostrar los resultados en el DOM
+  mostrarResultadosFiltradosNombreApellido(filtrados);
+});
+
+// Función para mostrar los resultados filtrados por nombre y/o apellido en el DOM
 function mostrarResultadosFiltradosNombreApellido(empleadosFiltrados) {
   const resultadosContainer = document.getElementById('resultadosFiltradosNombreApellido');
   resultadosContainer.innerHTML = '';
@@ -214,37 +253,3 @@ function mostrarResultadosFiltradosNombreApellido(empleadosFiltrados) {
     resultadosContainer.textContent = 'No se encontraron empleados con el nombre y/o apellido especificado.';
   }
 }
-
-
-// innerText
-document.addEventListener('DOMContentLoaded', function() {
-    const titulo = document.getElementById('title');
-    titulo.innerText = "Bienvenido al Cotizador de Alpina ART - Aseguradora de Riesgo del Trabajo";
-});
-
-// append
-let subtitulo  = document.createElement("h2")
-subtitulo.innerHTML = "¡Somos la Mejor ART del Mercado!"
-document.body.append(subtitulo)
-
-// Convertir empleados a JSON y guardar en localStorage
-function guardarEmpleadosEnLocalStorage() {
-    const empleadosJson = JSON.stringify(empleadosarts);
-    localStorage.setItem('empleados', empleadosJson);
-}
-
-// Recuperar empleados desde localStorage y convertir a objetos JavaScript
-function recuperarEmpleadosDeLocalStorage() {
-    const empleadosJson = localStorage.getItem('empleados');
-    if (empleadosJson) {
-      empleadosarts = JSON.parse(empleadosJson);
-    } else {
-        // Inicializar como un array vacío si no hay datos en localStorage
-      empleadosarts = [];
-    }
-}
-  
-
-  
-
-
